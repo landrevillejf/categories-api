ea49c3e71e84f6ca561fab2508a96e8a4f7e9697
Jean-Francois Landreville <landrevillejf@protonmail.com>
2023-09-26 10:28:21 -0400

    javadoc comments

--- src/main/java/com/protonmail/landrevillejf/cognos/categories/api/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/protonmail/landrevillejf/cognos/categories/api/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,6 @@
 package com.protonmail.landrevillejf.cognos.categories.api.service.impl;
 
 
-import com.protonmail.landrevillejf.cognos.categories.api.controller.CategoryController;
 import com.protonmail.landrevillejf.cognos.categories.api.entity.model.Category;
 import com.protonmail.landrevillejf.cognos.categories.api.exception.ApiExceptionEnums;
 import com.protonmail.landrevillejf.cognos.categories.api.exception.common.CommonApiException;
@@ -23,47 +22,91 @@ import java.util.Optional;
 @Transactional
 @Service
 public class CategoryServiceImpl implements ICommonService<Category> {
-    Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+    /**
+     *
+     */
+    private final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
+    /**
+     *
+     */
     private final CategoryRepository repository;
 
-    public CategoryServiceImpl(CategoryRepository repository) {
+    /**
+     *
+     * @param repository
+     */
+    public CategoryServiceImpl(final CategoryRepository repository) {
         this.repository = repository;
     }
 
     //region Find Category
+    /**
+     *
+     * @param page
+     * @param limit
+     * @return
+     */
     @Override
     public List<Category> findAll(int page, int limit) {
-        if(page >0) page -=1;
-        Pageable pageable= PageRequest.of(page, limit, Sort.by(Sort.Direction.ASC,"id"));
-        Page<Category> categoryPage= repository.findAll(pageable);
-
+        Page<Category> categoryPage = null;
+        if (page > 0) {
+            page -= 1;
+            Pageable pageable = PageRequest.of(page, limit, Sort.by(Sort.Direction.ASC, "id"));
+            categoryPage = repository.findAll(pageable);
+        }
+        assert categoryPage != null;
         return categoryPage.getContent();
     }
 
+    /**
+     *
+     * @param page
+     * @param limit
+     * @param search
+     * @return
+     */
     @Override
-    public List<Category> findAllByCriteria(int page, int limit, String search) {
-        if(page>0) page -=1;
-        Pageable pageable= PageRequest.of(page, limit, Sort.by(Sort.Direction.ASC,"id"));
-        Page<Category> featureTogglePage = null;
-        if(search ==null){
-            featureTogglePage= repository.findAll(pageable);
+    public List<Category> findAllByCriteria(int page, final int limit, final String search) {
+        Pageable pageable = null;
+        Page<Category> categories = null;
+        try {
+            if (page > 0) {
+                page -= 1;
+                pageable = PageRequest.of(page, limit, Sort.by(Sort.Direction.ASC, "id"));
+            }
+            if (search == null) {
+                assert pageable != null;
+                categories = repository.findAll(pageable);
+            } else if (search.isEmpty()) {
+                assert pageable != null;
+                categories = repository.findAll(pageable);
+            } else {
+                categories = repository.findByNameContaining(pageable, search);
+            }
+        } catch (Exception e) {
+            logger.error("Error: {}", e.getMessage(), e);
         }
-        else if(search.isEmpty()){
-            featureTogglePage= repository.findAll(pageable);
-        }else{
-            featureTogglePage= repository.findByNameContaining(pageable,search);
-        }
-        assert featureTogglePage != null;
-        return featureTogglePage.getContent();
+        assert categories != null;
+        return categories.getContent();
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @Override
     public Category findById(long id) {
         Optional <Category> category = repository.findById(id);
         return category.orElse(null);
     }
 
+    /**
+     *
+     * @param uid
+     * @return
+     */
     @Override
     public Category findByUid(String uid) {
         return repository.findByUid(uid);
@@ -71,12 +114,18 @@ public class CategoryServiceImpl implements ICommonService<Category> {
     //endregion
 
     //region Save Category
+
+    /**
+     *
+     * @param entity
+     * @return
+     */
     @Override
     public Category save(Category entity) {
         String categoryName=(entity.getName());
 
         Category getCategory= repository.findByName(categoryName);
-        if(getCategory != null){
+        if (getCategory != null){
             logger.error(ApiExceptionEnums.OBJECT_EXIST_EXCEPTION.name());
             throw new CommonApiException(entity.getName() + " " + ApiExceptionEnums.OBJECT_EXIST_EXCEPTION.name());
         }
@@ -95,10 +144,16 @@ public class CategoryServiceImpl implements ICommonService<Category> {
     //endregion
 
     //region Update Category
+
+    /**
+     *
+     * @param entity
+     * @return
+     */
     @Override
     public Category update(Category entity) {
-        Category category=findByUid(entity.getUid());
-        if(category == null){
+        Category category = findByUid(entity.getUid());
+        if (category == null){
             logger.error(ApiExceptionEnums.OBJECT_NOT_FOUND.name());
             throw new CommonApiException(entity.getName() + " " + ApiExceptionEnums.OBJECT_NOT_FOUND.name());
         }
@@ -113,10 +168,16 @@ public class CategoryServiceImpl implements ICommonService<Category> {
         return repository.save(category);
     }
 
+    /**
+     *
+     * @param entity
+     * @param uid
+     * @return
+     */
     @Override
     public Category update(Category entity, String uid) {
-        Category category=findByUid(uid);
-        if(category == null){
+        Category category = findByUid(uid);
+        if (category == null){
             logger.error(ApiExceptionEnums.OBJECT_NOT_FOUND.name());
             throw new CommonApiException(entity.getName() + " " + ApiExceptionEnums.OBJECT_NOT_FOUND.name());
         }
@@ -133,30 +194,42 @@ public class CategoryServiceImpl implements ICommonService<Category> {
     //endregion
 
     //region Delete Category
+
+    /**
+     *
+     * @param entity
+     */
     @Override
     public void delete(Category entity) {
         int categoryListSize= repository.findAll().size();
-        if(categoryListSize <1) {
+        if (categoryListSize < 1) {
             logger.error(ApiExceptionEnums.LIST_ALREADY_EMPTY.name());
             throw new CommonApiException(entity.getName() + " " + ApiExceptionEnums.LIST_ALREADY_EMPTY.name());
         }
         repository.delete(entity);
     }
 
+    /**
+     *
+     * @param uid
+     */
     @Override
     public void deleteByUid(String uid) {
-        Category category=findByUid(uid);
-        if(category == null){
+        Category category = findByUid(uid);
+        if (category == null){
             logger.error(ApiExceptionEnums.OBJECT_NOT_FOUND.name());
             throw new CommonApiException(uid + " " + ApiExceptionEnums.OBJECT_NOT_FOUND.name());
         }
         repository.delete(category);
     }
 
+    /**
+     *
+     */
     @Override
     public void deleteAll() {
-        int size=repository.findAll().size();
-        if(size <1) {
+        int size = repository.findAll().size();
+        if (size < 1) {
             logger.error(ApiExceptionEnums.LIST_ALREADY_EMPTY.name());
             throw new CommonApiException(ApiExceptionEnums.LIST_ALREADY_EMPTY.name());
         }
@@ -165,6 +238,11 @@ public class CategoryServiceImpl implements ICommonService<Category> {
     //endregion
 
     //region Count Category
+
+    /**
+     *
+     * @return
+     */
     @Override
     public long count() {
         return repository.count();
