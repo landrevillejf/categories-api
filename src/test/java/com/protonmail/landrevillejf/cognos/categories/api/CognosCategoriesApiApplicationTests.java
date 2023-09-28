a265e4c7d131a5dd2c6b55f37da7cadb5fb145f0
Jean-Francois Landreville <landrevillejf@protonmail.com>
2023-09-28 10:11:52 -0400

    add filters & exceptions

--- src/test/java/com/protonmail/landrevillejf/cognos/categories/api/CognosCategoriesApiApplicationTests.java
+++ src/test/java/com/protonmail/landrevillejf/cognos/categories/api/CognosCategoriesApiApplicationTests.java
@@ -3,7 +3,12 @@ package com.protonmail.landrevillejf.cognos.categories.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
 @SpringBootTest
+@ContextConfiguration(classes = { CognosCategoriesApiApplicationTests.class, TestConfig.class })
 class CognosCategoriesApiApplicationTests {
 
     @Test
@@ -11,3 +16,4 @@ class CognosCategoriesApiApplicationTests {
     }
 
 }
+
--- src/test/java/com/protonmail/landrevillejf/cognos/categories/api/TestConfig.java
+++ src/test/java/com/protonmail/landrevillejf/cognos/categories/api/TestConfig.java
@@ -1,5 +1,6 @@
 package com.protonmail.landrevillejf.cognos.categories.api;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +11,10 @@ public class TestConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public CircuitBreaker circuitBreaker() {
+        return CircuitBreaker.ofDefaults("myCircuitBreakerName");
     }
 }
