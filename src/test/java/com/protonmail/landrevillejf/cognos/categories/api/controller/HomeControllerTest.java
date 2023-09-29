1d091a0c5df9206eb0799674d7b1c96ce76d7d75
Jean-Francois Landreville <landrevillejf@protonmail.com>
2023-09-29 17:12:29 -0400

    test / redirect to /swagger-ui/index.html

--- src/test/java/com/protonmail/landrevillejf/cognos/categories/api/controller/HomeControllerTest.java
+++ src/test/java/com/protonmail/landrevillejf/cognos/categories/api/controller/HomeControllerTest.java
@@ -1,12 +1,24 @@
 package com.protonmail.landrevillejf.cognos.categories.api.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 class HomeControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void index() {
+    void index() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection()) // Expects a redirection
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/swagger-ui/index.html")); // Expects redirection URL
     }
 }
