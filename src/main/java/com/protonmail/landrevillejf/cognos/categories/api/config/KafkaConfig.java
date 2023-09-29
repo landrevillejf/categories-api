7fe8c8dd2832c5bcbd58462e44e98b315dfc2ed1
Jean-Francois Landreville <landrevillejf@protonmail.com>
2023-09-29 13:24:53 -0400

    Kafka Integration

---
+++ src/main/java/com/protonmail/landrevillejf/cognos/categories/api/config/KafkaConfig.java
@@ -0,0 +1,96 @@
+/**
+ * Kafka configuration class for production profile.
+ * This configuration class sets up the necessary Kafka producer and consumer configurations
+ * for connecting to a Kafka broker running on the specified bootstrap server(s).
+ *
+ * @Profile("production") Indicates that this configuration is active only in the "production" profile.
+ * @EnableKafka Enables Kafka integration in the Spring application.
+ */
+package com.protonmail.landrevillejf.cognos.categories.api.config;
+
+import com.protonmail.landrevillejf.cognos.categories.api.util.annotation.documentation.Author;
+import com.protonmail.landrevillejf.cognos.categories.api.util.annotation.documentation.Maintainer;
+import com.protonmail.landrevillejf.cognos.categories.api.util.annotation.documentation.Revision;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("CheckStyle")
+@Author(name = "Jean-Francois Landreville",
+        enterprise = "Lanaforge Inc.",
+        email = "landrevillejf@protonmail.com",
+        website = "https://www.lanaforge.ca"
+)
+@Maintainer(name = "Jean-Francois Landreville",
+        enterprise = "Lanaforge Inc.",
+        email = "landrevillejf@protonmail.com",
+        website = "https://www.lanaforge.ca"
+)
+@Revision(
+        date = "2023-09-29",
+        revision = 1,
+        comments = "KafkaConfig"
+)
+@Profile("production")
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    // Kafka broker's address
+    private final String bootstrapServers = "localhost:9092";
+
+    /**
+     * Configure the Kafka producer factory.
+     *
+     * @return The configured Kafka producer factory.
+     */
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    /**
+     * Create a Kafka template for sending messages to Kafka topics.
+     *
+     * @return The Kafka template configured for producing messages.
+     */
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    /**
+     * Configure the Kafka consumer factory.
+     *
+     * @return The configured Kafka consumer factory.
+     */
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        configProps.put(JsonDeserializer.VALUE_DEFAULT_TYPE, String.class.getName());
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+}
+
