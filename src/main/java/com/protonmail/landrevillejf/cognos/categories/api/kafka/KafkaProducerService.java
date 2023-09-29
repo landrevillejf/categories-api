bb58017db73dc3fd0fbd79b01ebb49ea5a06aacd
Jean-Francois Landreville <landrevillejf@protonmail.com>
2023-09-29 13:34:52 -0400

    Kafka Integration

---
+++ src/main/java/com/protonmail/landrevillejf/cognos/categories/api/kafka/KafkaProducerService.java
@@ -0,0 +1,39 @@
+package com.protonmail.landrevillejf.cognos.categories.api.kafka;
+
+import com.protonmail.landrevillejf.cognos.categories.api.util.annotation.documentation.Author;
+import com.protonmail.landrevillejf.cognos.categories.api.util.annotation.documentation.Maintainer;
+import com.protonmail.landrevillejf.cognos.categories.api.util.annotation.documentation.Revision;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@SuppressWarnings("CheckStyle")
+@Author(name = "Jean-Francois Landreville",
+        enterprise = "Lanaforge Inc.",
+        email = "landrevillejf@protonmail.com",
+        website = "https://www.lanaforge.ca"
+)
+@Maintainer(name = "Jean-Francois Landreville",
+        enterprise = "Lanaforge Inc.",
+        email = "landrevillejf@protonmail.com",
+        website = "https://www.lanaforge.ca"
+)
+@Revision(
+        date = "2023-09-29",
+        revision = 1,
+        comments = "KafkaProducerService"
+)
+@Profile("production")
+@Service
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String key, Object value) {
+        kafkaTemplate.send(topic, key, value);
+    }
+}
