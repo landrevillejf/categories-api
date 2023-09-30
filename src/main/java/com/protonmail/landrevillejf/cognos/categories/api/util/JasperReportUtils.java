5c1b5eb2611c68fbfe617aeedb1373e6f9a180a6
Jean-Francois Landreville <landrevillejf@protonmail.com>
2023-09-30 13:34:03 -0400

    JasperReportUtils, ReportExporter, ReportFiller classes added

---
+++ src/main/java/com/protonmail/landrevillejf/cognos/categories/api/util/JasperReportUtils.java
@@ -0,0 +1,98 @@
+package com.protonmail.landrevillejf.cognos.categories.api.util;
+
+import com.protonmail.landrevillejf.cognos.categories.api.util.jasperreport.SimpleReportExporter;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.HtmlExporter;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import net.sf.jasperreports.export.SimplePdfReportConfiguration;
+import net.sf.jasperreports.export.SimpleWriterExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JasperReportUtils {
+    /**
+     * Logger.
+     */
+    private final Logger logger = LoggerFactory.getLogger(JasperReportUtils.class);
+
+
+    private JasperPrint jasperPrint;
+
+    public void exportToPdf(String fileName, String author) {
+
+        // print report to file
+        JRPdfExporter exporter = new JRPdfExporter();
+
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(fileName));
+
+        SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+        reportConfig.setSizePageToContent(true);
+        reportConfig.setForceLineBreakPolicy(false);
+
+        SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+        exportConfig.setMetadataAuthor(author);
+        exportConfig.setEncrypted(true);
+        exportConfig.setAllowedPermissionsHint("PRINTING");
+
+        exporter.setConfiguration(reportConfig);
+        exporter.setConfiguration(exportConfig);
+        try {
+            exporter.exportReport();
+        } catch (JRException ex) {
+            logger.error("Error exporting data to pdf: {}", ex.getMessage(), ex);
+        }
+    }
+
+    public void exportToXlsx(String fileName, String sheetName) {
+        JRXlsxExporter exporter = new JRXlsxExporter();
+
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(fileName));
+
+        SimpleXlsxReportConfiguration reportConfig = new SimpleXlsxReportConfiguration();
+        reportConfig.setSheetNames(new String[] { sheetName });
+
+        exporter.setConfiguration(reportConfig);
+
+        try {
+            exporter.exportReport();
+        } catch (JRException ex) {
+            logger.error("Error exporting data to xlsx: {}", ex.getMessage(), ex);
+        }
+    }
+
+    public void exportToCsv(String fileName) {
+        JRCsvExporter exporter = new JRCsvExporter();
+
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleWriterExporterOutput(fileName));
+
+        try {
+            exporter.exportReport();
+        } catch (JRException ex) {
+            logger.error("Error exporting data to csv: {}", ex.getMessage(), ex);
+        }
+    }
+
+    public void exportToHtml(String fileName) {
+        HtmlExporter exporter = new HtmlExporter();
+
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleHtmlExporterOutput(fileName));
+
+        try {
+            exporter.exportReport();
+        } catch (JRException ex) {
+            logger.error("Error exporting data to html: {}", ex.getMessage(), ex);
+        }
+    }
+}
